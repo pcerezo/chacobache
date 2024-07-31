091d004e1bd2cbaf12298895462b10917e637bc8
pcerezo <pcerezo70@gmail.com>
2024-07-31 23:52:54 +0200

    Avance en la página de presentación. Añadido texto de presentación, animación de hover, imágenes de banner y de logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
