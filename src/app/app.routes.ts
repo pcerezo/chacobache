c0729815c6a54ca7a20d7de41a1db04385e463b2
pcerezo <pcerezo70@gmail.com>
2024-08-04 19:26:06 +0200

    Añadido ejemplo de código QR. Corrección en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { IntegrantesComponent } from './components/integrantes/integrantes.component';
 
 export const routes: Routes = [
-    {path: '', component: InicioComponent},
     {path: 'inicio', component: InicioComponent},
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     {path: 'integrantes', component: IntegrantesComponent},
-    
-    {path: '**', component: InicioComponent}
+    { path: '**', redirectTo: '/inicio' }
 ];
