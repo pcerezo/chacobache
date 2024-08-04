f9e55c0c7267b76b3eaeb1d9ea0e551e83fdd7f2
pcerezo <pcerezo70@gmail.com>
2024-08-04 18:54:32 +0200

    Añadida página de integrantes. Mostrados con el diseño card de Bootstrap.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { IntegrantesComponent } from './components/integrantes/integrantes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: InicioComponent},
+    {path: 'inicio', component: InicioComponent},
+    {path: 'integrantes', component: IntegrantesComponent},
+    
+    {path: '**', component: InicioComponent}
+];
