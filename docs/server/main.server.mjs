f37f2f639bd392d5aca8c960b49931a67aa740a9
pcerezo <pcerezo@users.noreply.github.com>
2025-03-03 22:00:16 +0000

    deploy: e08f9cf83dbe1f427f5f08bff6a41e1beea7318f

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WZJAOQOF.mjs";import"./chunk-7ELNYOSR.mjs";import"./chunk-VUXLGE6M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-SW76OBLL.mjs";import"./chunk-PCFR47SP.mjs";import"./chunk-VUXLGE6M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -7,7 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes, withHashLocation()), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
 };
 
 //export const BACKEND: String = "http://localhost:5000";
